ba17914feff492e992f4306785c5c371427028f5
Nicola Toledo <hello@nicolatoledo.dev>
2021-09-30 00:40:54 +0200

    about

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
-import Header from "components/header";
 import Page from "components/page";
 import type { NextPage } from "next";
+import About from "sections/home/about";
 import Hero from "sections/home/hero";
 
 const Home: NextPage = () => {
   return (
     <Page>
       <Hero />
+      <About/>
     </Page>
   );
 };
