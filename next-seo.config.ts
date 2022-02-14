19ba211dfbcd60c20028d57897f12b90f2409b72
Nicola Toledo <hello@nicolatoledo.dev>
2022-02-14 20:55:00 +0100

    added open graph image

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,13 +1,24 @@
 import { NextSeoProps } from "next-seo";
 
 const conf: NextSeoProps = {
-  title:'Nicola Toledo | dev & Trainer',
+  title:'Nicola Toledo | Developer & Trainer',
+  description:"I’ll turn your Figma, XD or Sketch file into a pixel-perfect Webflow Project ready to publish! 🚀",
   openGraph: {
     type: "website",
     locale: "it",
     url: "https://www.nicolatoledo.dev",
-    site_name: "Nicola Toledo | dev & Trainer",
-    title: "Nicola Toledo | dev & Trainer",
+    site_name: "Nicola Toledo | Developer & Trainer",
+    title: "Nicola Toledo | Developer & Trainer",
+    images:[
+      {
+        url: 'https://www.nicolatoledo.dev/open-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Og Image Alt',
+        type: 'image/png',
+      },
+    ],
+    description:"I’ll turn your Figma, XD or Sketch file into a pixel-perfect Webflow Project ready to publish! 🚀"
   },
   twitter: {
     handle: "@handle",
